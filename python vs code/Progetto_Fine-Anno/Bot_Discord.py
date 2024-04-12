48683d62073f22759fff01cc96804d9a3991b2d7
AlbertoBruscolini <147493117+AlbertoBruscolini@users.noreply.github.com>
2024-04-12 14:07:45 +0200

    Add files via upload
    Signed-off-by: AlbertoBruscolini <147493117+AlbertoBruscolini@users.noreply.github.com>

---
+++ python vs code/Progetto_Fine-Anno/Bot_Discord.py
@@ -0,0 +1,25 @@
+import os
+
+import discord
+from dotenv import load_dotenv
+
+load_dotenv()
+TOKEN = os.gatenv('DISCORD_TOKEN')
+
+intents = discord.Intents.default()
+intents.message_content = True
+
+client = discord.Client(intents=intents)
+
+@client.event
+async def on_ready():
+    print(f'Collegato come {client.user}')
+
+@client.event
+async def on_message(message):
+    if message.content.startswith( 'Come stai?'):
+        await message.channel.send ('Bene, graziel')
+client.run(TOKEN)
+
+#!!NON ANCORA CONCLUSO!!
+#settare i @client con botta e risposte, in caso caricare anche file
