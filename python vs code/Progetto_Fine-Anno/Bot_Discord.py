651c55ff21886210c6e803a0cd832707f1d4eb4c
AlbertoBruscolini <147493117+AlbertoBruscolini@users.noreply.github.com>
2024-04-12 14:18:32 +0200

    Update Bot_Discord.py
    Signed-off-by: AlbertoBruscolini <147493117+AlbertoBruscolini@users.noreply.github.com>

--- python vs code/Progetto_Fine-Anno/Bot_Discord.py
+++ python vs code/Progetto_Fine-Anno/Bot_Discord.py
@@ -4,7 +4,7 @@ import discord
 from dotenv import load_dotenv
 
 load_dotenv()
-TOKEN = os.gatenv('DISCORD_TOKEN')
+TOKEN = os.getenv('DISCORD_TOKEN')
 
 intents = discord.Intents.default()
 intents.message_content = True
