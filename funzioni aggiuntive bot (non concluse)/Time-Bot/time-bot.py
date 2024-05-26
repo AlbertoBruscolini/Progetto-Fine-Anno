40180362f1c13609f47fe95771bc3581eb9aad6d
AlbertoBruscolini <147493117+AlbertoBruscolini@users.noreply.github.com>
2024-05-26 14:17:51 +0200

    Update time-bot.py
    Signed-off-by: AlbertoBruscolini <147493117+AlbertoBruscolini@users.noreply.github.com>

--- funzioni aggiuntive bot (non concluse)/Time-Bot/time-bot.py
+++ funzioni aggiuntive bot (non concluse)/Time-Bot/time-bot.py
@@ -1,1 +1,28 @@
+from datetime import datetime      #MODULI DA AGGIUNGERE AGLI ALTRI, QUESTO CODICE CREA PROBLEMATICHE
+import pytz
 
+client = discord.Client()
+
+@client.event
+async def on_ready():
+    print('The bot is ready!')
+
+@client.event
+async def on_message(message):
+    if message.content.startswith('!time'):
+        continents = {
+            'africa': 'Africa/Cairo',
+            'antarctica': 'Antarctica/Casey',
+            'asia': 'Asia/Kolkata',
+            'australia': 'Australia/Sydney',
+            'europe': 'Europe/Paris',
+            'north_america': 'America/New_York',
+            'south_america': 'America/Sao_Paulo'
+        }
+        await message.channel.send('In quale continente vuoi l\'ora?')
+        response = await client.wait_for('message', check=lambda message: message.author == message.author and message.channel == message.channel)
+        if response.content in continents:
+            time = datetime.now(pytz.timezone(continents[response.content]))
+            await message.channel.send(f'L\'ora in {response.content} è: {time.strftime("%H:%M:%S")}')
+        else:
+            await message.channel.send('Continente non valido')
